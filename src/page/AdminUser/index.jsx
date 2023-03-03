6f52d9bbb77b07737401fadbf6e4aa469370c1d8
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-03-03 12:48:30 +0800

    Update AdminUser

--- src/page/AdminUser/index.jsx
+++ src/page/AdminUser/index.jsx
@@ -1,33 +1,81 @@
 import * as React from "react";
-import { DashboardLayout, Datatable } from "@/component";
-import Button from "@mui/material/Button";
+import { DashboardLayout, Datatable, Breadcrumb } from "@/component";
 import { useGetUsersQuery } from "@/state/api/reducer";
+import { useNavigate } from "react-router-dom";
 
 function AdminUser() {
-  const { data = {}, isLoading } = useGetUsersQuery();
-  const headers = ["ID", "First Name", "Last Name", "Email"];
-  const keys = ["id", "first_name", "last_name", "email"];
+  const navigate = useNavigate();
+  const { isLoading } = useGetUsersQuery();
+  const headers = ["ID", "First Name", "Last Name", "Email", "Type"];
+  const keys = ["_id", "firstName", "lastName", "email", "type"];
+
+  const [data, setData] = React.useState([
+    {
+      _id: "_id_1",
+      firstName: "firstName",
+      lastName: "lastName",
+      email: "email",
+      type: "type",
+    },
+    {
+      _id: "_id_2",
+      firstName: "firstName",
+      lastName: "lastName",
+      email: "email",
+      type: "type",
+    },
+    {
+      _id: "_id_3",
+      firstName: "firstName",
+      lastName: "lastName",
+      email: "email",
+      type: "type",
+    },
+    {
+      _id: "_id_4",
+      firstName: "firstName",
+      lastName: "lastName",
+      email: "email",
+      type: "type",
+    },
+    {
+      _id: "_id_5",
+      firstName: "firstName",
+      lastName: "lastName",
+      email: "email",
+      type: "type",
+    },
+  ]);
+
+  const handleDelete = (_id) => {
+    setData(data.filter((item) => item._id !== _id));
+  };
+
+  const handleEdit = (_id) => {
+    navigate(`/dashboard/users/edit/${_id}`);
+  };
 
   const actions = [
     {
-      onClick: (_id) => {},
+      onClick: handleEdit,
       title: "Edit",
     },
-    { onClick: (_id) => {}, title: "Delete" },
+    {
+      onClick: handleDelete,
+      title: "Delete",
+    },
   ];
-
-  if (isLoading) return <></>;
 
   return (
     <>
       <DashboardLayout>
-        <Button onClick={() => {}}>Add User</Button>
+        <Breadcrumb />
         <Datatable
           headers={headers}
           keys={keys}
           actions={actions}
           hasActions={true}
-          data={data.data}
+          data={data}
         />
       </DashboardLayout>
     </>
