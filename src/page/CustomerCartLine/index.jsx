2c12702daa17fdeff8c87194cd96d298c197bb43
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-03-03 12:48:56 +0800

    Add CustomerCartLine

---
+++ src/page/CustomerCartLine/index.jsx
@@ -0,0 +1,72 @@
+import * as React from "react";
+import { DashboardLayout, Datatable } from "@/component";
+import { useNavigate } from "react-router-dom";
+
+function CustomerCartLine() {
+  const navigate = useNavigate();
+  const headers = ["ID", "Product_id", "Cart_id"];
+  const keys = ["_id", "Product_id", "Cart_id"];
+
+  const [data, setData] = React.useState([
+    {
+      _id: "_id_1",
+      Product_id: "Product_id",
+      Cart_id: "Cart_id",
+    },
+    {
+      _id: "_id_2",
+      Product_id: "Product_id",
+      Cart_id: "Cart_id",
+    },
+    {
+      _id: "_id_3",
+      Product_id: "Product_id",
+      Cart_id: "Cart_id",
+    },
+    {
+      _id: "_id_4",
+      Product_id: "Product_id",
+      Cart_id: "Cart_id",
+    },
+    {
+      _id: "_id_5",
+      Product_id: "Product_id",
+      Cart_id: "Cart_id",
+    },
+  ]);
+
+  const handleDelete = (_id) => {
+    setData(data.filter((item) => item._id !== _id));
+  };
+
+  const handleEdit = (_id) => {
+    navigate(`/dashboard/cart-line/edit/${_id}`);
+  };
+
+  const actions = [
+    {
+      onClick: handleEdit,
+      title: "Edit",
+    },
+    {
+      onClick: handleDelete,
+      title: "Delete",
+    },
+  ];
+
+  return (
+    <>
+      <DashboardLayout>
+        <Datatable
+          headers={headers}
+          keys={keys}
+          actions={actions}
+          hasActions={true}
+          data={data}
+        />
+      </DashboardLayout>
+    </>
+  );
+}
+
+export default CustomerCartLine;
