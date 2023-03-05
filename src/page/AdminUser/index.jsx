f7366126fb0e1360a4f26a4a178419d3d8abb600
Renzo Cabarios <renzo.cabarios@gmail.com>
2023-03-05 14:00:19 +0800

    Update AdminUser

--- src/page/AdminUser/index.jsx
+++ src/page/AdminUser/index.jsx
@@ -1,58 +1,20 @@
 import * as React from "react";
-import { DashboardLayout, Datatable, Breadcrumb } from "@/component";
+import { DashboardLayout, Datatable, Breadcrumb, Button } from "@/component";
 import { useGetUsersQuery } from "@/state/api/reducer";
 import { useNavigate } from "react-router-dom";
 
 function AdminUser() {
   const navigate = useNavigate();
-  const { isLoading } = useGetUsersQuery();
+  const { data, isLoading } = useGetUsersQuery();
   const headers = ["ID", "First Name", "Last Name", "Email", "Type"];
-  const keys = ["_id", "firstName", "lastName", "email", "type"];
-
-  const [data, setData] = React.useState([
-    {
-      _id: "_id_1",
-      firstName: "firstName",
-      lastName: "lastName",
-      email: "email",
-      type: "type",
-    },
-    {
-      _id: "_id_2",
-      firstName: "firstName",
-      lastName: "lastName",
-      email: "email",
-      type: "type",
-    },
-    {
-      _id: "_id_3",
-      firstName: "firstName",
-      lastName: "lastName",
-      email: "email",
-      type: "type",
-    },
-    {
-      _id: "_id_4",
-      firstName: "firstName",
-      lastName: "lastName",
-      email: "email",
-      type: "type",
-    },
-    {
-      _id: "_id_5",
-      firstName: "firstName",
-      lastName: "lastName",
-      email: "email",
-      type: "type",
-    },
-  ]);
+  const keys = ["id", "first_name", "last_name", "email", "type"];
 
   const handleDelete = (_id) => {
-    setData(data.filter((item) => item._id !== _id));
+    // setData(data.filter((item) => item._id !== _id));
   };
 
   const handleEdit = (_id) => {
-    navigate(`/dashboard/users/edit/${_id}`);
+    // navigate(`/dashboard/users/edit/${_id}`);
   };
 
   const actions = [
@@ -66,16 +28,26 @@ function AdminUser() {
     },
   ];
 
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <>
       <DashboardLayout>
         <Breadcrumb />
+        <Button
+          title="Add User"
+          onClick={() => {
+            navigate("/dashboard/users/create");
+          }}
+        />
         <Datatable
           headers={headers}
           keys={keys}
           actions={actions}
           hasActions={true}
-          data={data}
+          data={data.data}
         />
       </DashboardLayout>
     </>
