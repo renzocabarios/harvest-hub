e106e12012ed191d1a31ed7fa2c9f728013593b4
Renzo Cabarios <renzo.cabarios@gmail.com>
2023-03-05 14:00:38 +0800

    Barrel Button in components

--- src/component/index.js
+++ src/component/index.js
@@ -5,8 +5,10 @@ import ListItem from "./ListItem";
 import Datatable from "./Datatable";
 import Breadcrumb from "./Breadcrumb";
 import UserLayout from "./UserLayout";
+import Button from "./Button";
 
 export {
+  Button,
   UserLayout,
   Breadcrumb,
   Datatable,
