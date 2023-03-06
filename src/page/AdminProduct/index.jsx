3db5c33b4f042b217bd0696994ae61e2f9eba80b
Renzo Cabarios <renzo.cabarios@gmail.com>
2023-03-06 12:25:13 +0800

    Update errors in AdminUser

--- src/page/AdminProduct/index.jsx
+++ src/page/AdminProduct/index.jsx
@@ -3,16 +3,32 @@ import { DashboardLayout, Datatable } from "@/component";
 import { useGetProductsQuery } from "@/state/api/reducer";
 
 function AdminProduct() {
-  const headers = ["ID", "Name", "Description", "Price"];
-  const keys = ["id", "name", "description", "price"];
+  const headers = ["ID", "Name", "Description", "Price", "Farmer"];
 
+  const keys = [
+    {
+      key: "id",
+    },
+    {
+      key: "name",
+    },
+    {
+      key: "description",
+    },
+    {
+      key: "price",
+      operation: (value, row) => `PHP ${value}`,
+    },
+    {
+      key: "farmer.user.first_name",
+      operation: (value, row) => `${value} ${row.farmer.user.last_name}`,
+    },
+  ];
   const getProducts = useGetProductsQuery();
 
   const handleDelete = (_id) => {};
 
-  const handleEdit = (_id) => {
-    // navigate(`/dashboard/product/edit/${_id}`);
-  };
+  const handleEdit = (_id) => {};
 
   const actions = [
     {
