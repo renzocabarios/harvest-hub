35f5e93eac56374fb6dbb683a4fdd4650e962fc3
Renzo Cabarios <renzo.cabarios@gmail.com>
2023-03-07 15:43:25 +0800

    Add ProductDetailLayout

---
+++ src/component/ProductDetailLayout.jsx
@@ -0,0 +1,32 @@
+import { Box, Paper } from "@mui/material";
+import ProductImage from "./ProductImage";
+import ProductDetails from "./ProductDetails";
+
+export default function (props) {
+  const { data } = props;
+  return (
+    <>
+      <Paper
+        sx={{
+          mb: 4,
+        }}
+      >
+        <Box
+          sx={{
+            p: 5,
+            display: "flex",
+            justifyContent: "space-evenly",
+            gap: 5,
+          }}
+        >
+          <ProductImage image={"https://source.unsplash.com/random"} />
+          <ProductDetails
+            name={data.name}
+            price={data.price}
+            description={data.description}
+          />
+        </Box>
+      </Paper>
+    </>
+  );
+}
